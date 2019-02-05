18516e245b24025df0792306bfcb1c240c05e91b
mehdad <m.mohamadi313@gmail.com>
2019-02-05 13:41:09 +0330

    The Miracle Begins

---
+++ ssz-sample-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: پلاگین نمونه
+Plugin URI: http://sitesazz.ir
+Version: 0.3
+Description: از این پلاگین به عنوان boilerplate برای پلاگین های دیگر استفاده میشود
+Author: مهرداد محمدی
+Author URI: https://www.linkedin.com/in/mehdad/
+Text Domain: wp-sample-plugin
+Domain Path: /languages
+*/
+
